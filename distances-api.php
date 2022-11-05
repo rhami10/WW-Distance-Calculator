a6e45a0f9168dbc28609eb63f470a2e23d2c8508
rhami1 <hami159@gmail.com>
2022-11-05 23:37:40 +0100

    Distances API bug fixes. Added CORS control, removed unnecessary object, updated response JSON and new 400 response headers for invalid requests

--- distances-api.php
+++ distances-api.php
@@ -1,94 +1,88 @@
 <?php
 
-    http_response_code(200);
-    header("Content-Type:application/json");
+    ini_set("display_errors",1);
+    error_reporting(E_ALL);
+
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header('Access-Control-Allow-Headers: *');
+    header('Content-Type: application/json');
 
     $fieldIds = ['distanceOneValue', 'distanceOneMetric', 'distanceTwoValue', 'distanceTwoMetric', 'finalDistanceMetric'];
-    $metersSelectOption = 'Meters';
-    $yardsSelectOption = 'Yards';
-    $conversionYMRate = 0.9144;
 
-    class Calculator {
-
-        global $metersSelectOption;
-        global $yardsSelectOption;
-        global $conversionYMRate;
-
-        public $distanceOneValue;
-        public $distanceOneMetric;
-        public $distanceTwoValue;
-        public $distanceTwoMetric;
-        public $finalDistanceMetric;
-
-        public function __construct($distanceOneValue, $distanceOneMetric, $distanceTwoValue, $distanceTwoMetric, $finalDistanceMetric) {
-            $this->$distanceOneValue = $distanceOneValue;
-            $this->$distanceOneMetric = $distanceOneMetric;
-            $this->$distanceTwoValue = $distanceTwoValue;
-            $this->$distanceTwoMetric = $distanceTwoMetric;
-            $this->$finalDistanceMetric = $finalDistanceMetric;
-        }
-
-        public function validateDistanceValues() {
-            return (is_numeric($distanceOneValue) && is_numeric($distanceTwoValue))
-        }
-
-        public function calculateFinalDistance() {
-            
-            if($finalDistanceMetric = $metersSelectOption) {
-                // Ensuring both values are in Meters
-                if($distanceOneMetric != $finalDistanceMetric) $distanceOneValue = $distanceOneValue * $conversionYMRate
-                if($distanceTwoMetric != $finalDistanceMetric) $distanceTwoValue = $distanceTwoValue * $conversionYMRate
-            }
-            else if ($finalDistanceMetric = $yardsSelectOption){
-                // Ensuring both values are in Yards
-                if($distanceOneMetric != $finalDistanceMetric) $distanceOneValue = $distanceOneValue / $conversionYMRate
-                if($distanceTwoMetric != $finalDistanceMetric) $distanceTwoValue = $distanceTwoValue / $conversionYMRate
-            }
-            else
-                return 0;
-
-            return $distanceOneValue + $distanceTwoValue;
-        }
-    }
-
-    function initialSetCheck () {
+    
+    // 
+    function initialSetCheck() {
         $isValid;
 
+        $_POST = json_decode(file_get_contents("php://input"), true);
+
+        global $fieldIds;
+
         foreach ($fieldIds as $fieldId) {
-            $isValid = (isset($_POST[$fieldId]) && $_POST[$fieldId]!="")
-            if($isValid) return false;
+            $isValid = (isset($_POST[$fieldId]) && $_POST[$fieldId]!="");
+            if(!$isValid) return false;
         }
         return true;
     }
 
+    //
     function sendResponse($success, $finalDistance, $feedbackTitle, $feedbackDescription) {
         
         $response;
 
         if($success)
-            $response = array('success' => $success, 'feedback_title' => $feedbackTitle, 'feedback_description' => $feedbackDescription);
+            $response = array('success' => $success, 'final_distance' => $finalDistance, 'feedback_title' => $feedbackTitle, 'feedback_description' => $feedbackDescription);
         else
-            $response = array('success' => $success, 'final_distance' => $finalDistance);
+            $response = array('success' => $success, 'feedback_title' => $feedbackTitle, 'feedback_description' => $feedbackDescription);
         
-        echo json_encode($response);
+            echo json_encode($response);
     }
 
+    //
+    function calculateFinalDistance($d1_value, $d1_metric, $d2_value, $d2_metric, $final_metric) {
+            
+        if($final_metric == 'Meters') {
+            // Ensuring both values are in Meters
+            if($d1_metric != $final_metric) $d1_value = $d1_value * 0.9144;
+            if($d2_metric != $final_metric) $d2_value = $d2_value * 0.9144;
+        }
+        else if ($final_metric == 'Yards'){
+            // Ensuring both values are in Yards
+            if($d1_metric != $final_metric) $d1_value = $d1_value / 0.9144;
+            if($d2_metric != $final_metric) $d2_value = $d2_value / 0.9144;
+        }
+        else
+            return 0;
+
+        return $d1_value + $d2_value;
+    }
 
     // Main program initialising a new Calculator Object and calling all methods required for the calculation.
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(initialSetCheck() == true) {
 
-    if(initialSetCheck == true) {
+            global $fieldIds;
+            
+            $distanceOneValue = $_POST[$fieldIds[0]];
+            $distanceOneMetric = $_POST[$fieldIds[1]];
+            $distanceTwoValue = $_POST[$fieldIds[2]];
+            $distanceTwoMetric = $_POST[$fieldIds[3]];
+            $finalDistanceMetric = $_POST[$fieldIds[4]];
 
-        $calculator = new Calculator($_POST($fieldIds[0]), $_POST($fieldIds[1]), $_POST($fieldIds[2]), $_POST($fieldIds[3]), $_POST($fieldIds[4]))
+            if(is_numeric($distanceOneValue) && is_numeric($distanceTwoValue)) { // Checking if both values are valid.
+                sendResponse(true, calculateFinalDistance(floatval($distanceOneValue), $distanceOneMetric, floatval($distanceTwoValue), $distanceTwoMetric, $finalDistanceMetric), "Success", "Results generated!");
+            } 
+            else {
+                header("HTTP/1.1 400 Bad Request");
+                sendResponse(false, null, "Invalid Inputs", "Distance values are invalid.");
+            }
 
-        if($calculator->validateDistanceValues) {
-            sendResponse(true, $calculator->calculateFinalDistance(), "Success", "Results generated!")
-        } 
-        else {
-            sendResponse(false, null, "Invalid Inputs", "Distance values are invalid.")
+        } else {
+            header("HTTP/1.1 400 Bad Request");
+            sendResponse(false, null, "Missing Values", "Please make sure all input fields are set.");
+            exit();
         }
-
-    } else {
-        sendResponse(false, null, "Missing Values", "Please make sure all input fields are set.")
-    } 
+    }
     
 ?>
