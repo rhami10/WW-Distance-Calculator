9a7edc09ba3db52bcd5ccb9c73649adbf7ffce3d
rhami1 <hami159@gmail.com>
2022-11-05 20:13:25 +0100

    Added new function to get the total distance from PHP service. Replaced calculator class with object.

--- js/distanceCalculator.js
+++ js/distanceCalculator.js
@@ -1,53 +1,74 @@
-let METRIC_UNIT = ["Meters", "Yards"]; // Using a singular array variable to populate the form's Select Element options.
+let METRIC_UNIT = ['Meters', 'Yards']; // Using a singular array variable to populate the form's Select Element options.
 
-class DistanceCalculator {
-	constructor(
-		form,
-		distanceOneValue,
-		distanceOneMetric,
-		distanceTwoValue,
-		distanceTwoMetric,
-		finalDistanceMetric
-	) {
-		this.form = form;
-		this.distanceOneValue = distanceOneValue;
-		this.distanceOneMetric = distanceOneMetric;
-		this.distanceTwoValue = distanceTwoValue;
-		this.distanceTwoMetric = distanceTwoMetric;
-		this.finalDistanceMetric = finalDistanceMetric;
-	}
-
-	// Call and fetch the final distance result from the PHP function.
-
-	// Update the Feedback panel.
-
-	// Update the Results panel.
-}
-
-const form = document.querySelector("calculatorForm");
+const form = document.querySelector('calculatorForm');
 
 if (form) {
-	const calcultorObj = new Calculator(
-		form,
-		document.getElementById("distanceOneValue"),
-		document.getElementById("distanceOneMetric"),
-		document.getElementById("distanceTwoValue"),
-		document.getElementById("distanceTwoMetric"),
-		document.getElementById("finalDistanceMetric")
-	);
+
+    const calcObj = {
+        distanceOneValue: document.getElementById('distanceOneValue'),
+        distanceOneMetric: document.getElementById('distanceOneMetric'),
+        distanceTwoValue: document.getElementById('distanceTwoValue'),
+        distanceTwoMetric: document.getElementById('distanceTwoMetric'),
+        finalDistanceMetric: document.getElementById('finalDistanceMetric')
+    }
+    
 
 	// Populating Select form elements
 	METRIC_UNIT.forEach((metricText) => {
-		var newOption = document.createElement("option");
+		var newOption = document.createElement('option');
 		newOption.textContent = metricText;
 		newOption.value = metricText;
-		calcultorObj.distanceOneMetric.appendChild(newOption);
-		calcultorObj.distanceTwoMetric.appendChild(newOption);
-		calcultorObj.finalDistanceMetric.appendChild(newOption);
+
+		calcObj.distanceOneMetric.appendChild(newOption);
+		calcObj.distanceTwoMetric.appendChild(newOption);
+		calcObj.finalDistanceMetric.appendChild(newOption);
 	});
 
-	form.addEventListener("submit", function (evt) {
+	form.addEventListener('submit', function (evt) {
 		evt.preventDefault();
-		calculatorObj.getFinalDistance();
+		getFinalDistance();
 	});
+}
+
+// Call and fetch the final distance result from the PHP function.
+getFinalDistance() {
+    fetch('//localhost/DistanceCalculator/distances-api', {
+        method: 'POST',
+        body: JSON.stringify(calcObj),
+        header: {
+            'Content-type': 'application/json'
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if(data.success) {
+            // Update the results panel with the new value;
+            document.getElementById('finalDistanceValue').innerHTML = data.final_distance;
+            updateFeedbackPanel(true, null, null);
+        }
+        else {
+            document.getElementById('finalDistanceValue').innerHTML = "";
+            updateFeedbackPanel(false, data.feedback_title, data.feedback_description);
+        }
+    });
+}
+
+// Update the Feedback panel.
+updateFeedbackPanel(success, title, description) {
+
+    var textColor, panelColor;
+
+    if(success) {
+        textColor = "#365E3D";
+        panelColor = "#D6F5DB";
+    } else {
+        textColor = "#660000";
+        panelColor = "#F5D9BC";
+    }
+
+    document.getElementById('feedbackTitle').innerHTML = title;
+    document.getElementById('feedbackText').innerHTML = description;
+    document.getElementById('feedback-panel').style.backgroundColor = panelColor;
+    document.getElementById('feedbackTitle').style.color = textColor;
+    document.getElementById('feedbackText').style.color = textColor;
 }
